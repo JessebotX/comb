c470242cad672f71f3c7ce67f55ebebdfcd544b0
Jesse <jessebot.git@gmail.com>
2025-08-13 15:00:00 -0700

    feat: parse first level (non-command) flags

--- comb.go
+++ comb.go
@@ -3,6 +3,7 @@ package comb
 import (
 	"fmt"
 	"reflect"
+	"strconv"
 	"strings"
 )
 
@@ -11,8 +12,6 @@ type Context struct{}
 func Parse(args []string, cli any) (Context, error) {
 	unparsedArgs := make([]string, len(args))
 	copy(unparsedArgs, args)
-
-	fmt.Printf("%#v\n", unparsedArgs)
 
 	rValue := reflect.ValueOf(cli).Elem()
 	rType := reflect.TypeOf(cli).Elem()
@@ -24,36 +23,73 @@ func Parse(args []string, cli any) (Context, error) {
 			continue
 		}
 
-		if err := set(arg, rValue, rType); err != nil {
+		if err := set(arg, &unparsedArgs, i, rValue, rType); err != nil {
 			return Context{}, err
 		}
-
-		*arg = ""
 	}
-
-	fmt.Printf("%#v\n", unparsedArgs)
 
 	return Context{}, nil
 }
 
-func set(arg *string, cliReflectValue reflect.Value, cliReflectType reflect.Type) error {
+func set(arg *string, unparsed *[]string, currentArgIndex int, cliReflectValue reflect.Value, cliReflectType reflect.Type) error {
 	for i := 0; i < cliReflectType.NumField(); i++ {
 		field := cliReflectType.Field(i)
-
-		if strings.EqualFold(*arg, "-h") || strings.EqualFold(*arg, "-help") {
-			fmt.Print("FLAG: <SPECIAL HELP FLAG>\n")
-			return nil
-		}
+		fieldReflectValue := cliReflectValue.FieldByName(field.Name)
 
 		cmdName, ok := field.Tag.Lookup("cmd")
 		if ok && strings.EqualFold(*arg, cmdName) {
-			fmt.Printf("CMD:   %s\n", cmdName)
+			fmt.Printf("(TODO) CMD: %s\n", cmdName)
 			return nil
 		}
 
 		flagName, ok := field.Tag.Lookup("flag")
 		if ok && strings.EqualFold(*arg, "-"+flagName) {
-			fmt.Printf("FLAG: -%s\n", flagName)
+			if !fieldReflectValue.CanSet() {
+				return fmt.Errorf("flag \"%s\": cannot be given a value", *arg)
+			}
+
+			switch fieldReflectValue.Kind() {
+			case reflect.Bool:
+				fieldReflectValue.SetBool(true)
+				*arg = ""
+			case reflect.String:
+				if (currentArgIndex + 1) >= len(*unparsed) {
+					return fmt.Errorf("flag \"%s\": missing argument of type 'string'", *arg)
+				}
+
+				fieldReflectValue.SetString((*unparsed)[currentArgIndex+1])
+				*arg = ""
+				(*unparsed)[currentArgIndex+1] = ""
+			case reflect.Int:
+				if (currentArgIndex + 1) >= len(*unparsed) {
+					return fmt.Errorf("flag \"%s\": missing argument of type 'int'", *arg)
+				}
+
+				parsedArg, err := strconv.ParseInt((*unparsed)[currentArgIndex+1], 10, 64)
+				if err != nil {
+					return fmt.Errorf("flag \"%s\": %w", parsedArg)
+				}
+
+				fieldReflectValue.SetInt(parsedArg)
+				*arg = ""
+				(*unparsed)[currentArgIndex+1] = ""
+			case reflect.Float64:
+				if (currentArgIndex + 1) >= len(*unparsed) {
+					return fmt.Errorf("flag \"%s\": missing argument of type 'int'", *arg)
+				}
+
+				parsedArg, err := strconv.ParseFloat((*unparsed)[currentArgIndex+1], 64)
+				if err != nil {
+					return fmt.Errorf("flag \"%s\": %w", parsedArg)
+				}
+
+				fieldReflectValue.SetFloat(parsedArg)
+				*arg = ""
+				(*unparsed)[currentArgIndex+1] = ""
+			default:
+				return fmt.Errorf("flag \"%s\" unsupported field type %v", *arg, fieldReflectValue.Type())
+			}
+
 			return nil
 		}
 	}
--- example/comb/main.go
+++ example/comb/main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"log"
+
 	"github.com/JessebotX/comb"
 )
 
 var cli struct {
-	Init    InitCommand `cmd:"init" help:"Initialize a project"`
-	Verbose bool        `flag:"verbose" help:"Print debug information"`
+	Init      InitCommand `cmd:"init" help:"Initialize a project"`
+	Verbose   bool        `flag:"verbose" help:"Print debug information"`
+	TestFloat float64     `flag:"test-float" help:"Test float value"`
+	TestStr   string      `flag:"test-str" help:"Test string value"`
+	TestInt   int         `flag:"test-int" help:"Test int value"`
 }
 
 type InitCommand struct {
@@ -21,7 +26,18 @@ func main() {
 		"-init-threads",
 		"2",
 		"init",
+		"-test-float",
+		"16.25",
+		"-test-int",
+		"16",
+		"-test-str",
+		"16",
 	}
 
-	_, _ = comb.Parse(args, &cli)
+	_, err := comb.Parse(args, &cli)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("%+v\n", cli)
 }
